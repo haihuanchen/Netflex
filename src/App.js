5f2372991b9af08f7bedf8c6303abd0f322060ea
haihuanchen <haihuan.chen@hotmail.com>
2021-03-31 18:06:48 -0400

    added movies posters and scroll function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Front-end of Netflex App</h1>
-      <Row title="Netflex Originals" />
-      <Row title="Trending Now" />
+      <Row title="Netflex Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
+import "./Row.css";
 
-function Row({ title }) {
+const baseURL= "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
 
+    //A snippet of code which runs based on a specific condition
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    },[fetchUrl]);
+
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
 
-            {/* container for the posters*/}
-            <div>
-
+            <div className="row-posters">
+                {movies.map(movie => (
+                    <img key={movie.id} className="row-poster" src={`${baseURL}${movie.poster_path}`} alt={movie.name}/>
+                ))}
             </div>
         </div>
     )
