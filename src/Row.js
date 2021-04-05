f99da406fc5a8132c1f90abdf1256b7f065ec275
haihuanchen <haihuan.chen@hotmail.com>
2021-04-05 15:29:48 -0400

    added trailers to movie posters

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import "./Row.css";
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const baseURL= "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(()=>{
         async function fetchData(){
@@ -16,6 +19,28 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     },[fetchUrl]);
 
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if(trailerUrl) {
+            setTrailerUrl('');
+        }else{
+            movieTrailer(movie?.name || "")
+                .then((url)=>{
+                    //https://www.youtube.com/watch?v=imGaOIm5HOk&ab_channel=YirumaVEVO
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get("v"));
+                })
+                .catch((error)=> console.log(error));
+        }
+    }
+
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -24,12 +49,14 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 {movies.map(movie => (
                     <img 
                         key={movie.id} 
+                        onClick={() => handleClick(movie)}
                         className={`row-poster ${isLargeRow && "row-posterLarge"}` }
                         src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name}
                     />
                 ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
